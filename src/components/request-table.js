8dc852368c4019e5eb3b8f3aadc756b2736de55a
Jon Meyers <jonathon.d.meyers@gmail.com>
2018-05-24 12:38:43 +1000

    FIX: Use moment library instead of manual time calculation

--- src/components/request-table.js
+++ src/components/request-table.js
@@ -2,17 +2,9 @@ import React from 'react';
 import { Table, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import Moment from 'moment';
 
 const RequestTable = props => {
-  const timeSince = time => {
-    const now = new Date();
-    const then = new Date(time);
-    const difference = now.getTime() - then.getTime();
-    const mins = new Date(difference).getMinutes();
-    const phrasing = mins === 1 ? 'min' : 'mins';
-    const msg = mins > 0 ? `${mins} ${phrasing} ago` : 'just now';
-    return msg;
-  };
   const renderRow = request => (
     <Table.Row key={request.id}>
       <Table.Cell>{request.name}</Table.Cell>
@@ -20,7 +12,7 @@ const RequestTable = props => {
       <Table.Cell>
         <ReactMarkdown source={request.desc} />
       </Table.Cell>
-      <Table.Cell>{timeSince(request.timeRequested)}</Table.Cell>
+      <Table.Cell>{Moment(request.timeRequested).fromNow()}</Table.Cell>
       <Table.Cell
         className="clickable request-row-trash"
         onClick={() => props.closeRequest(request.id)} // eslint-disable-line react/prop-types
